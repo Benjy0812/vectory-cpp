0b031cf0d21fbe25942034f7a9bb1ed191298f34
Benjy <108727379+Benjy0812@users.noreply.github.com>
2026-09-05 17:38:25 +0200

    WIP add loop that works occasionally there is something with cin taking printed output as input and other small updates

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
-#include  <iostream>
+#include<iostream>
+#include<string>
+#include<vector>
+
 using std::cout;
 using std::cin;
 using std::endl;
+using std::vector;
+using std::string;
 
-static int show_list() {
+static int show_list(const vector<string>& list) {
     cout << "\nList";
+    for (const string& item : list)
+        cout << item << endl;
     return 0;
 }
 
-static int add_list() {
+void add_list(vector<string>& list) {
+
     cout << "\nAdd to list";
-    return 0;
+    string itemName;
+    cin >> itemName;
+
+    list.push_back(itemName);
 }
 
 static int remove_list() {
@@ -29,29 +40,36 @@ static int exit_cli() {
 }
 
 int main() {
-    cout << "Welcome!";
-    cout << "\n1. Show list";
-    cout << "\n2. Add to list";
-    cout << "\n3. Remove from list";
-    cout << "\n4. purge list";
-    cout << "\n5. Exit";
-    cout << "\nEnter your choice: ";
+    vector<string> list;
+    bool state = false;
 
-    int choice;
-    cin >> choice;
+    while (state == false) {
+        cout << "Welcome!";
+        cout << "\n1. Show list";
+        cout << "\n2. Add to list";
+        cout << "\n3. Remove from list";
+        cout << "\n4. purge list";
+        cout << "\n5. Exit";
+        cout << endl;
+        cout << "\nEnter your choice: ";
 
-    switch (choice) {
-        case 1: show_list();
-            break;
-        case 2: add_list();
-            break;
-        case 3: remove_list();
-            break;
-        case 4: purge_list();
-            break;
-        case 5: exit_cli();
-            break;
-        default: cout << "\nInvalid choice!";
+        int choice;
+        cin >> choice;
+
+        switch (choice) {
+            case 1: show_list(list);
+                break;
+            case 2: add_list(list);
+                break;
+            case 3: remove_list();
+                break;
+            case 4: purge_list();
+                break;
+            case 5: exit_cli();
+                state = true;
+                break;
+            default: cout << "\nInvalid choice!";
+        }
     }
     return 0;
 }
